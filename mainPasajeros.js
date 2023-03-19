51879d858eb1a978099bf6078ed57803d0769e6f
sebaprograma <mmo.sebastian@gmail.com>
2023-03-19 15:32:03 -0300

    borro archivos que no se usan se pasaron al mainPasajeros y hago pruebas json consumiendo datos locales

--- mainPasajeros.js
+++ mainPasajeros.js
@@ -1,6 +1,22 @@
-const deportista = {
-    nombre: 'Messi',
-    champions: 4,
-};
+const deportistaObjeto = {
+    nombre:"Messi",
+    champions:4};
+const deportista1 = '{"nombre":"Ronaldo","champions":3}';
 
-console.log(deportista);
+
+console.log(deportistaObjeto);
+console.log(JSON.stringify(deportistaObjeto));
+console.log(deportista1);
+console.log(JSON.parse(deportista1));
+
+fetch('http://127.0.0.1:5500/pasajeros.json')
+.then((Response) => Response.json())
+.then((data) => console.log(data));
+
+fetch('http://127.0.0.1:5500/pasajeros.json')
+.then((Response) => Response.json())
+.then((pasajeros) => {
+    const nombresDePasajeros = pasajeros.
+    map((pasajero) => pasajero.nombre);
+    console.log(nombresDePasajeros);
+});
